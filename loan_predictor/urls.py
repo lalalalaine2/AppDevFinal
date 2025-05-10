019ab95c2e8dd82be994fbdcda07ba9199edb7ed
lalalainemay <hz202300493@wmsu.edu.phm>
2025-05-10 22:25:07 +0800

    better log out

--- loan_predictor/urls.py
+++ loan_predictor/urls.py
@@ -4,6 +4,7 @@ from predictor import views
 from django.contrib.auth.views import LogoutView, LoginView
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
+from django.contrib.auth import views as auth_views
 
 def redirect_if_not_authenticated(request):
     if not request.user.is_authenticated:
@@ -17,6 +18,6 @@ urlpatterns = [
     path('accounts/register/', views.register, name='register'),
     path('predict/', login_required(views.predict), name='predict'),
     path('dashboard/', login_required(views.dashboard), name='dashboard'),
-    path('logout/', LogoutView.as_view(next_page='home'), name='home'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
 ]
